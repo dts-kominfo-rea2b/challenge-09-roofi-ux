ee9e270cfbd5747eeec0efdf74e32d8bb919e449
Firdha Roofi Irawan <roofiii96@gmail.com>
2022-06-20 15:53:29 +0700

    Update Jawaban

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <div className="container">
+        <Header />
+        <div className="contact-container">
+          <Contact data={contacts[0]} />
+          <Contact data={contacts[1]} />
+          <Contact data={contacts[2]} />
+          <Contact data={contacts[3]} />
+          <Contact data={contacts[4]} />
+          <Contact data={contacts[5]} />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  const { name, email, phone, photo } = data;
+  return (
+    <div className="contacts-wrapper">
+      <div className="avatar-wrapper">
+        <img src={photo} alt="avatar" />
+      </div>
+      <div className="info-wrapper">
+        <p className="info-name">{name}</p>
+        <div className="info-content">
+          <p className="info-content_phone">{phone}</p>
+          <p className="info-content-email">{email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
